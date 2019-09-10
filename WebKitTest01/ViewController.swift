81618c7b5b10c071ccdc572353ac6a382416bb82
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-09-10 13:54:01 +0900

    webkit

--- WebKitTest01/ViewController.swift
+++ WebKitTest01/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let request = URLRequest(url : URL(string: "http://www.google.com")!)
+        webView.load(request)
     }
 
 
